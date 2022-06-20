ed71d739b93dade332cdc14ac5a7a257191d7a64
AidanKimani <aidan.kimani@student.moringaschool.com>
2022-06-20 10:50:07 +0300

    commit tree 8494c9caaedb35bf117b2a3e3e5a5fdc3806205b

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  for(let start = 0; start < word.length / 2; start++){
+    const end = word.length -1 - start;
+    if(word[start] === word[end]) return true;
+    return false;
+  };
+};
 
 /* 
   Add your pseudocode here
+    iterate from the start of the string to the middle of the string
+    compare the letters we're iterating over to the letter at the end of the string
+    and if we reach the middle and the letters match, return true;
+    else false
 */
 
 /*
   Add written explanation of your solution here
+    isPalindrome is a function that returns either true or false when
+  the input string is the same forwards and backwards, it should return true, i.e,
+  if the first letter is the same as the last letter up to the middle, return true
+  else if the first and the last don't match return false.
 */
 
 // You can run `node index.js` to view these console logs
